e27e5a7011c6dcc391f084b1e0b52487d8da931d
Oleksandr Kutsenko <104622826+Fstv-Electro@users.noreply.github.com>
2023-07-13 19:45:25 +0300

    save

---
+++ src/components/HeaderNav.vue
@@ -0,0 +1,52 @@
+
+<script>
+
+export default {
+    name: 'HeaderNav',
+    components: {},
+}
+</script>
+
+
+
+<template>
+    <div class="header">
+        <p class="logo">
+            P&D CENTER
+        </p>
+        <nav class="nav-links">
+            <router-link to="/">Home</router-link>
+        </nav>
+    </div>
+
+</template>
+
+<style>
+
+
+.header {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 16px;
+  height: 64px;
+  background-color: #211f1f;
+}
+
+.logo {
+  color: white;
+  /* margin-right: auto; */
+}
+
+.nav-links {
+    /* margin-right: auto; */
+}
+
+.nav-links a.router-link-exact-active{
+  color: #211f1f;
+  background-color: white;
+  padding: 8px;
+  border-radius: 8px;
+}
+
+</style>
---
+++ src/views/HomePage.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="banner">
+        <h1 class="banner__title">Здоров'я також впливає на вашу красу.</h1>
+        <button class="banner__button">Продукція</button>
+    </div>
+    <div class="container">
+        <p class="welcome">Ласкаво просимо до нашого центру подології та краси! Ми пропонуємо широкий спектр послуг для догляду за ногами та тілом, які допоможуть вам виглядати і почуватися найкращим чином.
+        Наші досвідчені фахівці використовують найсучасніші технології та методи, щоб забезпечити вам ефективний і комфортний догляд. Ми пропонуємо широкий спектр послуг, включаючи:</p>
+        <div class="podology">
+            <h2 class="podology__title">Подологію</h2>
+            <img src="../images/podology.jpg" alt="" class="podology__img">
+        </div>
+        <div class="manicure">
+            <h2 class="manicure__title">Подологію</h2>
+            <img src="../images/green.jpg" alt="" class="podology__img">
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'HomePage',
+    components: {},
+}
+</script>
+
+<style>
+
+</style>
