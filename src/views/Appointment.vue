87d252c98de53661cd5b2b232946a2c1c65944e4
Oleksandr Kutsenko <104622826+Fstv-Electro@users.noreply.github.com>
2023-08-31 16:22:37 +0300

    add form

--- src/views/Appointment.vue
+++ src/views/Appointment.vue
@@ -10,7 +10,20 @@ export default {
     <div class="container">
         <h2 class="title">Запис на прийом</h2>
         <div class="appointment-container">
-            
+            <form action="" class="user-form">
+                <label for="">
+                    <input type="name" placeholder="ім'я">
+                </label>
+                <label for="">
+                    <input type="text" placeholder="телефон">
+                </label>
+                <label for="">
+                    <input type="text" placeholder="текст">
+                </label>
+            </form>
+            <div class="chat-container">
+                
+            </div>
         </div>
     </div>
 </template>
